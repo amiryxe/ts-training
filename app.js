6f28037c8da51609bfcc6f7ba0d2bd59f5e08550
Amir Salehi <nik.amirsa@gmail.com>
2022-12-03 17:01:32 +0330

    types for objects

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+// types for function parameters
 function add(num1, num2) {
     return num1 + num2;
 }
 var result = add(5, 8);
 console.log(result);
+// types for objects
+var myObject = {
+    title: 'tv',
+    price: 1200
+};
+console.log(myObject.title);
--- app.ts
+++ app.ts
@@ -1,7 +1,19 @@
+// types for function parameters
 function add(num1: number, num2: number) {
     return num1 + num2
 }
 
-const result = add(5, 8)
+const result = add(5, 9)
 
 console.log(result)
+
+// types for objects
+const myObject: {
+    title: string
+    price: number
+} = {
+    title: 'tv',
+    price: 1200
+}
+
+console.log(myObject.title)
